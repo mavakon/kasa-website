55047278909e87f8a84824a2c091b04191f41881
6alahad <dgosan397@gmail.com>
2023-11-27 21:28:11 +0200

    prod_tamplate#1
    зробив контроллер і теймплейт з тимчасовими фотками для відображення, пізніше треба буде переписати, коли буде повністю готова база даних

--- src/main/java/com/bdsk/kasa/web/HomeController.java
+++ src/main/java/com/bdsk/kasa/web/HomeController.java
@@ -1,7 +1,8 @@
 package com.bdsk.kasa.web;
 
-
+import com.bdsk.kasa.domain.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,5 +11,18 @@ public class HomeController {
     public String home() {
         return "home";
 
+    }
+
+     @GetMapping("/product")
+    public String showProduct(Model model) {
+        Product product = new Product();
+        product.setName("Sample Product");
+        product.setDescription("This is a sample product description.");
+        product.setPrice(20);
+        product.setAuthor("John Doe");
+
+        model.addAttribute("product", product);
+
+        return "product";
     }
 }
