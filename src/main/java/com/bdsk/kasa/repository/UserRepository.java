4297f9b0270a8cfd7f7649bee22ab96a527a5a05
mavakon <maksim.m.konshin@gmail.com>
2023-11-30 00:15:00 +0200

    feat: Switch to using JSON Arrays

--- src/main/java/com/bdsk/kasa/repository/UserRepository.java
+++ src/main/java/com/bdsk/kasa/repository/UserRepository.java
@@ -2,153 +2,65 @@ package com.bdsk.kasa.repository;
 
 import com.bdsk.kasa.domain.User;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.springframework.stereotype.Repository;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.*;
 
 @Repository
 public class UserRepository implements GenericRepository<User, Integer> {
 
-    private final Map<Integer, Long> indexMap = new HashMap<>();
     private final String filePath = "users.json";
     private final Gson gson = new Gson();
 
-    public UserRepository() {
-        buildIndexMap();
-    }
-
-    private void buildIndexMap() {
-        indexMap.clear();
-        try (RandomAccessFile file = new RandomAccessFile(filePath, "r")) {
-            long position = 0;
-            String line;
-            while ((line = file.readLine()) != null) {
-                User user = gson.fromJson(line, User.class);
-                indexMap.put(user.getId(), position);
-                position = file.getFilePointer();
-            }
-        } catch (IOException e) {
-            // TODO Add exception handling
-        }
-    }
-
+    @Override
     public User save(User userToSave) {
-        List<User> allUsers = new ArrayList<>();
-        boolean isUserToSavePresent = false;
-        int idToSave = userToSave.getId();
+        List<User> allUsers = findAll();
+        allUsers.removeIf(user -> user.getId() == userToSave.getId());
+        allUsers.add(userToSave);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                User user = gson.fromJson(line, User.class);
-                if (!(user.getId() == idToSave)) {
-                    allUsers.add(user);
-                } else {
-                    allUsers.add(userToSave);
-                    isUserToSavePresent = true;
-                }
-            }
+        try (Writer writer = new FileWriter(filePath)) {
+            gson.toJson(allUsers, writer);
         } catch (IOException e) {
-            // TODO Add exception handling
+            // TODO: Add exception handling
         }
-        if (!isUserToSavePresent) {
-            allUsers.add(userToSave);
-        }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            for (User user : allUsers) {
-                writer.write(gson.toJson(user));
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            // TODO Add exception handling
-        }
-        buildIndexMap();
         return userToSave;
     }
-
     @Override
     public Optional<User> findById(Integer id) {
-        if (!indexMap.containsKey(id)) {
-            return Optional.empty();
-        }
-        try (RandomAccessFile file = new RandomAccessFile(filePath, "r")) {
-            file.seek(indexMap.get(id));
-            User user = gson.fromJson(file.readLine(), User.class);
-            return Optional.of(user);
-        } catch (IOException e) {
-            // TODO Add exception handling
-            return Optional.empty();
-        }
-
+        return findAll().stream()
+                .filter(user -> user.getId() == id)
+                .findFirst();
     }
 
     public Optional<User> findByLogin(String login) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                User user = gson.fromJson(line, User.class);
-                if (user.getLogin().equals(login)) {
-                    return Optional.of(user);
-                }
-            }
-        } catch (IOException e) {
-            // TODO Add exception handling
-        }
-        return Optional.empty();
+        return findAll().stream()
+                .filter(user -> user.getLogin().equals(login))
+                .findFirst();
     }
 
     @Override
     public List<User> findAll() {
-        List<User> users = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                User user = gson.fromJson(line, User.class);
-                users.add(user);
-            }
+        try (Reader reader = new FileReader(filePath)) {
+            Type userListType = new TypeToken<ArrayList<User>>(){}.getType();
+            return gson.fromJson(reader, userListType);
         } catch (IOException e) {
-            // TODO Add exception handling
+            // TODO: Add exception handling
         }
-        return users;
+        return new ArrayList<>();
     }
 
     @Override
     public void deleteById(Integer id) {
+        List<User> allUsers = findAll();
+        allUsers.removeIf(user -> user.getId() == id);
 
-        if (!indexMap.containsKey(id)) {
-            return;
-        }
-
-        List<User> allUsers = new ArrayList<>();
-        boolean isUserFound = false;
-
-        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                User user = gson.fromJson(line, User.class);
-                if (!(user.getId() == id)) {
-                    allUsers.add(user);
-                } else {
-                    isUserFound = true;
-                }
-            }
+        try (Writer writer = new FileWriter(filePath)) {
+            gson.toJson(allUsers, writer);
         } catch (IOException e) {
-            // TODO Add exception handling
+            // TODO: Add exception handling
         }
-
-        if (!isUserFound) {
-            return;
-        }
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
-            for (User user : allUsers) {
-                writer.write(gson.toJson(user));
-                writer.newLine();
-            }
-        } catch (IOException e) {
-            // TODO Add exception handling
-        }
-
-        buildIndexMap();
     }
 }
